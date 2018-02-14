e04cc175b9f9acd67e73b669c0a52524182fd6ad
gianni963 <gianni_guatieri@hotmail.com>
2018-02-14 17:49:29 +0100

    contact.blade error message configuration

--- resources/views/informations/contact.blade.php
+++ resources/views/informations/contact.blade.php
@@ -7,21 +7,38 @@
 			<hr>
 			<form action="{{ route('postContact') }}" method="POST" >
 				{{ csrf_field() }}
-				<div class="form-group">
+
+				<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
 					<label name="email">Your email:</label>
 					<input id="email" name="email" class="form-control">
+					    @if ($errors->has('email'))
+                            <span class="help-block">
+                                     <strong>{{ $errors->first('email') }}</strong>
+                               </span>
+                        @endif
 				</div>			
 
-				<div class="form-group">
+				<div class="form-group{{ $errors->has('subject') ? ' has-error' : '' }}">
 					<label name="subject">Subject:</label>
 					<input id="subject" name="subject" class="form-control">
+					    @if ($errors->has('subject'))
+                            <span class="help-block">
+                                     <strong>{{ $errors->first('subject') }}</strong>
+                               </span>
+                        @endif					
 				</div>
 
-				<div class="form-group">
+				<div class="form-group{{ $errors->has('message') ? ' has-error' : '' }}">
 					<label name="message">Message:</label>
 					<textarea id="message" name="message" placeholer="write your message here" class="form-control"></textarea>
+						@if ($errors->has('message'))
+                            <span class="help-block">
+                                     <strong>{{ $errors->first('emessge') }}</strong>
+                               </span>
+                        @endif
 				</div>
 				<input type="submit" value="Send Message">
+				
 			</form>
 		</div>
 	</div>
